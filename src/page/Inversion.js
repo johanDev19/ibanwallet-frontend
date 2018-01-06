d27379ffc6cc879839bfb6eceb242be48cbaa1c6
Johan Guzman <johan.dev19@gmail.com>
2018-01-06 18:09:33 -0400

    page inversion updated

--- src/page/Inversion.js
+++ src/page/Inversion.js
@@ -3,7 +3,7 @@ import icon1 from './../img/Inversiones_screen_1.png'
 import icon2 from './../img/Rentabilidad.svg'
 import icon3 from './../img/Comisiones.svg'
 import icon4 from './../img/Accesible.svg'
-
+import icon5 from './../img/banner_07.png'
 
 class Inversion extends Component {
 
@@ -14,7 +14,7 @@ class Inversion extends Component {
                 <section className="inv-1-section  d-flex justify-content-center align-items-center">
                     <div className="d-flex justify-content-start  inv-1-section_container">
                         <div className="inv-1-section_text info-section_text d-flex flex-column align-items-start justify-content-center">
-                            <h4 className="">El modelo de inversión de los grandes inversores, al alcance de todos</h4>
+                            <h1 className="">El modelo de inversión de los grandes inversores, al alcance de todos</h1>
                             <ul>
                                 <li>La industria <strong>necesita una oferta diferente</strong>, más enfocada en el cliente y que sea clara y honesta.</li>
                                 <li>Necesita una novedosa manera invertir, con <strong>productos de bajo coste</strong>, sin conflictos de intereses y eliminando todo lo que no te aporta valor.</li>
@@ -57,17 +57,16 @@ class Inversion extends Component {
                     </div>
                 </section>
 
-                <section className="inv-4-section">
-                    <h4 className="text-center">HA LLEGADO EL MOMENTO</h4>
-                </section>
 
 
-                <section className="inv-3-section d-flex justify-content-end align-items-center ">
-                    <div className="inv-3-section_text d-flex justify-content-center align-items-center">
-                        <div className="container">
-                            <p className="text-left">Gracias a la tecnología de Internet hemos conseguido una reducción de costes en la relación con nuestros clientes y gestión interna que nos permite ofrecer un <strong>modelo de comisiones al alcance de todos.</strong> </p>
-                            <p>Esto es ya una realidad en países como Reino Unido. Creemos que ahora es el momento idóneo de su llegada a España, para utilizarlo en tu beneficio.</p>
-                        </div>
+                <section className="inv-3-section d-flex justify-content-between align-items-center container-fluid">
+                    <figure>
+                        <img src={icon5} alt=""/>
+                    </figure>
+                    <div className="inv-3-section_text">
+                        <h2 className="text-left">HA LLEGADO EL MOMENTO</h2>
+                        <p className="text-left">Gracias a la tecnología de Internet hemos conseguido una reducción de costes en la relación con nuestros clientes y gestión interna que nos permite ofrecer un <strong>modelo de comisiones al alcance de todos.</strong> </p>
+                        <p>Esto es ya una realidad en países como Reino Unido. Creemos que ahora es el momento idóneo de su llegada a España, para utilizarlo en tu beneficio.</p>
                     </div>
                 </section>
 
