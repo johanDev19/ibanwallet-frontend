c9099ed79d71137e254a6faaccbb81d7b12cd3b5
Johan Guzman <johan.dev19@gmail.com>
2018-01-08 15:54:35 -0400

    responsive update

--- src/component/Menu.js
+++ src/component/Menu.js
@@ -32,13 +32,13 @@ class Menu extends Component {
     render(){
         return(
             <header className={this.state.shadow === true?'d-flex justify-content-between align-items-center page-menu container-fluid shadow':'d-flex justify-content-between align-items-center page-menu container-fluid'}>
-                <figure className="col-2">
+                <figure className="col">
                     <a href="/">
                         <img src={logo} alt="logo" />
                     </a>
                 </figure>
-                <nav className="col-md col-lg col-sm-1 menu hidden-lg-down">
-                    <i className="fa fa-bars d-md-none d-lg-none d-sm-block" aria-hidden="true"></i>
+                <nav className="col-2 menu hidden-lg-down">
+                    <i className="fa fa-bars d-md-none d-lg-none d-sm-block float-right " aria-hidden="true"></i>
                     <ul className="justify-content-end d-none d-md-flex">
                         <a href="/iban">
                             <li>iBAN</li>
