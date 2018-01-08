a429a8408066c6c3e25c960c86ab32140711b161
Johan Guzman <johan.dev19@gmail.com>
2018-01-07 22:01:33 -0400

    se actualizo la imagen del home page del mobile

---
+++ src/page/Cookie.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react'
+
+class Cookie extends Component {
+    render(){
+        return(
+            <div className="cookie container-fluid">
+                <section className="coo-1-section container">
+                    <h1 className="h6 font-weight-bold">Terminos y condiciones de uso del sitio web <a href="www.myibanwallet.com">www.myibanwallet.com</a></h1>
+
+
+                </section>
+            </div>
+        )
+    }
+
+
+}
+
+export default Termino
