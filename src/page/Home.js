f0929f630414297b43ea8724d4723dc65d5b8c41
Johan Guzman <johan.dev19@gmail.com>
2018-01-08 14:23:43 -0400

    se avanzo la version mobile

--- src/page/Home.js
+++ src/page/Home.js
@@ -52,21 +52,42 @@ class Home extends Component {
                             <h3 className="font-weight-bold">Esa es la nueva forma de ahorrar</h3>
                         </div>
                     </header>
-                    <figure>
+                    <figure className="banner">
                         <a href="https://secure.myibanwallet.com">
                             <button>Abre tu cuenta</button>
                         </a>
                         <img src={banner_1} alt="intro" />
+                        <div className="info-1-section_img_mobile ">
+                            <div className="img-container">
+                                <figure className={"mobile-img d-flex justify-content-center align-items-center"}>
+                                    <div className="d-flex">
+                                        <img src={s_1} alt="card" />
+                                    </div>
+                                </figure>
+                            </div>
+
+                        </div>
                     </figure>
+
 
                 </section>
 
-                <section className="info-1-section d-flex align-items-center">
-                    <div className="d-flex justify-content-between container">
+                <section className="info-1-section d-flex align-items-lg-center align-items-start">
+                    <div className="d-flex justify-content-between container ">
                         <div className="info-1-section_text info-section_text d-flex flex-column align-items-start justify-content-center col-lg-6 col-md-12 col-sm-12">
                             <h2>iBAN te facilita toda TU actividad financiera de forma segura y sencilla.</h2>
                             <p>Traspasa su plan de pensiones o contrato los mejores fondos de inversión en tan solo 5 minutos.</p>
                             <span className='font-weight-bold'>Bienvenido a un mundo más allá de la banca, Bienvenido a <a href="">iBAN</a></span>
+                        </div>
+                        <div className="info-1-section_img_mobile ">
+                            <div className="img-container">
+                                <figure className={"mobile-img d-flex justify-content-center align-items-center"}>
+                                    <div className="d-flex">
+                                        <img src={s_2} alt="card" />
+                                    </div>
+                                </figure>
+                            </div>
+
                         </div>
                         <div className="info-1-section_img " style={{top: this.state.mobilePosition }}>
                             <div className="img-container">
@@ -82,11 +103,21 @@ class Home extends Component {
                         </div>
                     </div>
                 </section>
-                <section className="info-3-section d-flex align-items-center ">
+                <section className="info-3-section d-flex align-items-lg-center align-items-start">
                     <div className="d-flex justify-content-start container ">
                         <div className="info-1-section_text info-section_text d-flex flex-column align-items-start justify-content-center col-lg-6 col-md-12 col-sm-12">
                             <h2>Ayuda al instante</h2>
                             <p>Ponerse en contacto con nosotros es tan fácil como enviar un SMS o un Whatsapp a un amigo. ¡Nuestro equipo de soporte está siempre disponible para ayudar a través del chat en la aplicación!</p>
+                        </div>
+                        <div className="info-1-section_img_mobile ">
+                            <div className="img-container">
+                                <figure className={"mobile-img d-flex justify-content-center align-items-center"}>
+                                    <div className="d-flex">
+                                        <img src={s_2} alt="card" />
+                                    </div>
+                                </figure>
+                            </div>
+
                         </div>
                     </div>
                 </section>
