f771c6ba39d1c0b4cfc32ecf674ba0e88e8346f1
Johan Guzman <johan.dev19@gmail.com>
2018-01-08 20:36:06 -0400

    ahorro page resposive updated

--- src/page/Ahorro.js
+++ src/page/Ahorro.js
@@ -11,39 +11,45 @@ class Ahorro extends Component {
 
 
                 <section className="ah-2-section">
-                    <h1 className="text-center h2">¿Cómo funciona?</h1>
 
                     <div className="d-flex flex-column align-items-center">
+                        <h1 className="text-center h2">¿Cómo contratar un plan de ahorro?</h1>
+
                         <div className="ah-2-section_text d-flex align-items-center justify-content-around">
 
                             <div className="text">
-                                <h6 className="h2">1. Tus preferencias</h6>
-                                <span>PARA OFRECERTE EL PLAN QUE MEJOR SE ADAPTE A TI</span>
-                                <p>Dinos tus preferencias, con cuánto quieres empezar y cuánto puedes poner al mes. No te agobies porque podrás cambiarlo cuando quieras.</p>
+                                <h6 className="h2">Tus preferencias</h6>
+                                <span>PARA OFRECERTE EL PLAN QUE<span> MEJOR SE ADAPTE A TI</span></span>
+                                <p className="d-none d-ms-block d-md-block d-lg-block">Dinos tus preferencias, con cuánto quieres empezar y cuánto puedes poner al mes. No te agobies porque podrás cambiarlo cuando quieras.</p>
                             </div>
+
                             <figure>
                                 <img src={icon1} alt="ahorro1"/>
                             </figure>
+                            <p className="d-lg-none d-md-none d-ms-block text-left">Dinos tus preferencias, con cuánto quieres empezar y cuánto puedes poner al mes. No te agobies porque podrás cambiarlo cuando quieras.</p>
+
                         </div>
                         <div className="ah-2-section_text d-flex align-items-center flex-row-reverse justify-content-around ">
                             <div className="text">
-                                <h6 className="h2">2. Tu perfil de ahorrador</h6>
+                                <h6 className="h2">Tu perfil de ahorrador</h6>
                                 <span>PARA CONFIGURAR EL PLAN A TU MEDIDA</span>
-                                <p>Responde 10 sencillas preguntas sobre ti para que podamos calcular tu perfil de ahorrador y diseñar tu plan a medida.</p>
+                                <p className="d-none d-ms-block d-md-block d-lg-block">Responde 10 sencillas preguntas sobre ti para que podamos calcular tu perfil de ahorrador y diseñar tu plan a medida.</p>
                             </div>
                             <figure>
                                 <img src={icon2} alt="ahorro2"/>
                             </figure>
+                            <p className="d-lg-none d-md-none d-ms-block text-left">Responde 10 sencillas preguntas sobre ti para que podamos calcular tu perfil de ahorrador y diseñar tu plan a medida.</p>
                         </div>
                         <div className="ah-2-section_text d-flex align-items-center justify-content-around">
                             <div className="text">
-                                <h6 className="h2">3. Introduce tus datos personales</h6>
+                                <h6 className="h2">Introduce tus datos personales</h6>
                                 <span>Y ACTÍVALO 100% ONLINE</span>
-                                <p>Actívalo en 10 minutos y totalmente online, nuestra tecnología se encarga de todo.</p>
+                                <p className="d-none d-ms-block d-md-block d-lg-block">Actívalo en 10 minutos y totalmente online, nuestra tecnología se encarga de todo.</p>
                             </div>
                             <figure>
                                 <img src={icon3} alt="ahorro3"/>
                             </figure>
+                            <p className="d-lg-none d-md-none d-ms-block text-left">Actívalo en 10 minutos y totalmente online, nuestra tecnología se encarga de todo.</p>
                         </div>
                     </div>
 
@@ -51,11 +57,12 @@ class Ahorro extends Component {
                 </section>
 
                 <section className="ah-1-section row align-items-center justify-content-center">
+                    <h2 className=" m-text d-lg-none d-md-none d-ms-block text-left text-center">¿Por qué debería ahorrar con iBAN?</h2>
                     <figure className="col-4">
                         <img src={img1} alt=""/>
                     </figure>
                     <div className="ah-1-section_text col-md-6 col-sm-12">
-                        <h2>¿Por qué debería ahorrar con iBAN?</h2>
+                        <h2 className='d-none d-ms-block d-md-block d-lg-block'>¿Por qué debería ahorrar con iBAN?</h2>
                         <ol>
                             <li><strong>La inflación se come tus ahorros</strong>
                                 <p>
