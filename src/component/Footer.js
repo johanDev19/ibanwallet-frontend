c4fc40a25ff3e103ed7d5cbdd1091fb6a120c86a
Marc-Anthony Hurr <marcanthonyhurr@ibanonline.com>
2018-01-09 23:14:38 -0400

    fixes marc

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,5 +1,10 @@
 import React, {Component} from 'react'
 import logo from './../img/Logoibanwallet.svg'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 class Footer extends Component {
     render(){
@@ -17,18 +22,18 @@ class Footer extends Component {
                             </span>
                             <span className="r">® iBAN Wallet 2018</span>
                             <ul className="d-flex l-social">
-                                <a href="https://www.facebook.com/myibanwallet" target="_blank">
+                                <Link to="https://www.facebook.com/myibanwallet" target="_blank">
                                     <i className="fa fa-facebook" aria-hidden="true"></i>
-                                </a>
-                                <a href="https://twitter.com/myibanwallet" target="_blank">
+                                </Link>
+                                <Link to="https://twitter.com/myibanwallet" target="_blank">
                                     <i className="fa fa-twitter" aria-hidden="true"></i>
-                                </a>
-                                <a href="https://www.linkedin.com/company/11428519/" target="_blank">
+                                </Link>
+                                <Link to="https://www.linkedin.com/company/11428519/" target="_blank">
                                     <i className="fa fa-linkedin" aria-hidden="true"></i>
-                                </a>
-                                <a href="https://www.instagram.com/myibanwallet/" target="_blank">
+                                </Link>
+                                <Link to="https://www.instagram.com/myibanwallet/" target="_blank">
                                     <i className="fa fa-instagram" aria-hidden="true"></i>
-                                </a>
+                                </Link>
                             </ul>
                         </div>
 
@@ -85,7 +90,7 @@ class Footer extends Component {
                         </a>
                     </ul>
                     <p>
-                        iBAN Investment Group S.L., Paseo de las Delicias, 1, 41001, Sevilla, Agente de Esfera Capital, Agencia de valores sujeta a supervisión de la <a href="http://www.cnmv.es/Portal/Consultas/ESI/ESIS.aspx?nif=A04791943&id=660730&vista=19&fs=" target="_blank">CNMV</a> inscrito con el número 252. Agente de de Seguros Exclusivo de Caser (Caja de Seguros Reunidos, Compañia de Seguros y reaseguros, S.A.) sujeto a la supervisión de la DGSFP inscrito con el número C0031B90344383. Los planes de ahorro están gestionados por Caser y depositado en CecaBank.
+                        iBAN Investment Group S.L., Paseo de las Delicias, 1, 41001, Sevilla, Agente de Esfera Capital, Agencia de valores sujeta a supervisión de la <Link to="http://www.cnmv.es/Portal/Consultas/ESI/ESIS.aspx?nif=A04791943&id=660730&vista=19&fs=" target="_blank">CNMV</Link> inscrito con el número 252. Agente de de Seguros Exclusivo de Caser (Caja de Seguros Reunidos, Compañia de Seguros y reaseguros, S.A.) sujeto a la supervisión de la DGSFP inscrito con el número C0031B90344383. Los planes de ahorro están gestionados por Caser y depositado en CecaBank.
                     </p>
                 </div>
             </footer>
--- src/page/Iban.js
+++ src/page/Iban.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react'
 import MetaTags from 'react-meta-tags';
-
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 
 import icon1 from './../img/web_iban_imagen2.jpg'
 import icon2 from './../img/web_iban_imagen1.jpg'
@@ -76,11 +80,11 @@ class Iban extends Component {
                         <ul>
                             <li>
                                 “iBAN como uno de los actores principales en un sector que está asistiendo a una verdadera transformación sin precedentes” - <strong>
-                                <a href="https://cincodias.elpais.com/cincodias/2017/11/17/emprendedores/1510907958_535416.html" target="_blank">CincoDías</a></strong>
+                                <Link to="https://cincodias.elpais.com/cincodias/2017/11/17/emprendedores/1510907958_535416.html" target="_blank">CincoDías</Link></strong>
                             </li>
                             <li>
-                                ”iBAN, fintech enfocada a digitalizar los procesos complejos y convertirlos en procesos rápidos para todos sus clientes, tiene el objetivo de simplificar y automatizar tecnológicamente productos de inversión y ahorro para acercarlos al gran público” - <strong><a
-                                href="http://www.finanzas.com/noticias/noticias-corporativas/20171204/caser-iban-wallet-alian-3737450.html" target="_blank">Inversion y finanzas</a></strong>
+                                ”iBAN, fintech enfocada a digitalizar los procesos complejos y convertirlos en procesos rápidos para todos sus clientes, tiene el objetivo de simplificar y automatizar tecnológicamente productos de inversión y ahorro para acercarlos al gran público” - <strong><Link
+                                to="http://www.finanzas.com/noticias/noticias-corporativas/20171204/caser-iban-wallet-alian-3737450.html" target="_blank">Inversion y finanzas</Link></strong>
                             </li>
 
 
