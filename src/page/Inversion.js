26ca75f0296f44b007a1c9c414d32b57d6bc4592
Johan Guzman <johan.dev19@gmail.com>
2018-01-09 22:05:05 -0400

    updated

--- src/page/Inversion.js
+++ src/page/Inversion.js
@@ -59,7 +59,7 @@ class Inversion extends Component {
                             <img src={icon3} alt=""/>
                         </figure>
                         <p>COMISIONES MÍNIMAS</p>
-                        <p className="text-center">Un 80% menos de comisiones que la banca. Eliminamos comisiones.</p>
+                        <p className="text-center">La aplicación de la tecnología a la gestión de tus inversiones nos permite ofrecerte los mejores productos a un menor coste</p>
                     </div>
                 </div>
             ],
@@ -134,7 +134,7 @@ class Inversion extends Component {
                                 <img src={icon3} alt=""/>
                             </figure>
                             <p>COMISIONES MÍNIMAS</p>
-                            <p className="text-center">Un 80% menos de comisiones que la banca. Eliminamos comisiones.</p>
+                            <p className="text-center">La aplicación de la tecnología a la gestión de tus inversiones nos permite ofrecerte los mejores productos a un menor coste</p>
                         </div>
                     </div>
                 </section>
@@ -147,8 +147,9 @@ class Inversion extends Component {
                     </figure>
                     <div className="inv-3-section_text">
                         <h2 className="text-left">HA LLEGADO EL <span>MOMENTO</span></h2>
-                        <p className="text-left">Gracias a la tecnología de Internet hemos conseguido una reducción de costes en la relación con nuestros clientes y gestión interna que nos permite ofrecer un <strong>modelo de comisiones al alcance de todos.</strong> </p>
-                        <p>Esto es ya una realidad en países como Reino Unido. Creemos que ahora es el momento idóneo de su llegada a España, para utilizarlo en tu beneficio.</p>
+                        <p>Eliminar el texto de arriba y poner  "iBAN pone a disposición de sus clientes una forma más eficiente ​de gestionar sus ahorros. Productos de ahorro e inversión consistentes y rentables, adaptados al perfil de cada cliente. La mejor tecnología al servicio de los ahorros de nuestros clientes.</p>
+                        <p>Lo que es ya una realidad en otros países como Reino Unido al alcance de los ahorradores en España. Empieza a gestionar tus ahorros en iBAN y disfruta de una forma distinta de hacer las cosas</p>
+
                     </div>
                 </section>
 
